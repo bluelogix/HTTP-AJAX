0c5870fc8f1202a9f457b90665673b3ca1283ad2
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-01-23 17:56:22 -0500

    Displayed friends list from server

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
+// import { setupMaster } from 'cluster    ';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friendsList: [],
+      error: ''
+    }
+  }
+
+  componentDidMount() {
+    console.log("cdm working");
+    axios.get('http://localhost:5000/friends')
+    .then(res => {
+        this.setState({ friendsList: res.data})
+    })
+    .catch(err => {
+      this.setState({ error: err})
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        
+        {this.state.friendsList.map( friend => {
+          return(
+            <div>
+             <h1>{friend.name}</h1>
+              <p>{friend.age}</p>
+              <p>{friend.email}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
